cd02d9e6e978c420ea81fa5270e733b7a60dfec3
Thea Whatling <K038218@eltham-college.org.uk>
2020-09-24 12:44:14 +0100

    airport reading from file and splitting rows in to fields

--- Airport/Program.cs
+++ Airport/Program.cs
@@ -19,13 +19,26 @@ namespace Airport
             {
                 text = File.ReadAllText("Airports.txt");
                 // reads all text in Airports file
-
                 Console.WriteLine(text);
+                // prints the text in airports file
+
+                string[] rows = text.Split('\n');
+                Console.WriteLine(rows.Length);
+                Console.ReadLine();
+                //Console.WriteLine(rows[1]);   //writes row 1
+
+                string[] fields = rows.Split(',');
+                Console.WriteLine(fields.Length);
+
+
+
             }
             catch // if file is not found - its following code will not work
             {
                 Console.WriteLine("error - can not open file");
             }
+
+
         }
     }
 }
