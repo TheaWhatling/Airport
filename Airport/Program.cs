6b0a90fad8bce5d37c26008860a05ba30e059815
Thea Whatling <K038218@eltham-college.org.uk>
2020-09-24 12:20:13 +0100

    Add project files.

---
+++ Airport/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Airport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            read();
+        }
+        static void read()
+        {
+            string text;
+
+            try
+            {
+                text = File.ReadAllText("Airports.txt");
+                // reads all text in Airports file
+
+                Console.WriteLine(text);
+            }
+            catch // if file is not found - its following code will not work
+            {
+                Console.WriteLine("error - can not open file");
+            }
+        }
+    }
+}
