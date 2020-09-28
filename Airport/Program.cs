b1144e49080e4c43a158a9359406de2b9f49700e
Thea Whatling <K038218@eltham-college.org.uk>
2020-09-28 11:24:58 +0100

    errors fixed and arrays of fields can now be called globally in program

--- Airport/Program.cs
+++ Airport/Program.cs
@@ -5,11 +5,23 @@ namespace Airport
 {
     class Program
     {
+        static string[] code;
+        static string[] name;
+        static string[] distanceL;
+        static string[] distanceB;
+
         static void Main(string[] args)
         {
             Console.WriteLine("");
 
             read();
+
+            //for (int x = 0; x < code.Length; x = x + 1)
+            //{
+            //    Console.WriteLine(code[x]);
+            //}
+            // prints the field 'code'
+
         }
         static void read()
         {
@@ -28,10 +40,11 @@ namespace Airport
                 //Console.WriteLine(rows[1]);   //writes row 1
 
                 
-                string[] code = new string[rows.Length];
-                string[] name = new string[rows.Length];
-                string[] distanceL = new string[rows.Length];
-                string[] distanceB = new string[rows.Length];
+                code = new string[rows.Length];
+                name = new string[rows.Length];
+                distanceL = new string[rows.Length];
+                distanceB = new string[rows.Length];
+                // this creates 4 new arrays 
 
                 string[] fields;
                 for(int z = 0; z < rows.Length; z = z + 1)
@@ -41,10 +54,12 @@ namespace Airport
                     name[z] = fields[1];
                     distanceL[z] = fields[2];
                     distanceB[z] = fields[3];
+                    // each field has its own array filled with the airport files data
                 }
 
             }
-            catch // if file is not found - its following code will not work
+            catch 
+            // if file is not found - its following code will not work
             {
                 Console.WriteLine("error - can not open file");
             }
