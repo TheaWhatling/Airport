a1b19f08104132a82c995d76902dbb51054fc7f4
Thea Whatling <K038218@eltham-college.org.uk>
2020-09-28 11:12:26 +0100

    fields now separately stored in own arrays

--- Airport/Program.cs
+++ Airport/Program.cs
@@ -27,10 +27,21 @@ namespace Airport
                 Console.ReadLine();
                 //Console.WriteLine(rows[1]);   //writes row 1
 
-                string[] fields = rows.Split(',');
-                Console.WriteLine(fields.Length);
+                
+                string[] code = new string[rows.Length];
+                string[] name = new string[rows.Length];
+                string[] distanceL = new string[rows.Length];
+                string[] distanceB = new string[rows.Length];
 
-
+                string[] fields;
+                for(int z = 0; z < rows.Length; z = z + 1)
+                {
+                    fields = rows[z].Split(',');
+                    code[z] = fields[0];
+                    name[z] = fields[1];
+                    distanceL[z] = fields[2];
+                    distanceB[z] = fields[3];
+                }
 
             }
             catch // if file is not found - its following code will not work
